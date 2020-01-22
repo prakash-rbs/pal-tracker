dfb8fd583b76152c44402a9ccd755699b6452b9e
pal_user <Nuti.Prakash@rbs.co.uk>
2020-01-22 15:11:51 +0530

    Assignment5_MYSQL_CFServices

---
+++ databases/tracker/migrations/V1__initial_schema.sql
@@ -0,0 +1,13 @@
+
+use tracker_test
+CREATE TABLE time_entries (
+  id         BIGINT NOT NULL AUTO_INCREMENT,
+  project_id BIGINT,
+  user_id    BIGINT,
+  date       DATE,
+  hours      INT,
+
+  PRIMARY KEY (id)
+)
+  ENGINE = innodb
+  DEFAULT CHARSET = UTF8MB4;
